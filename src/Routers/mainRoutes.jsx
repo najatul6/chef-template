334328861b59f13e94d62e5075c4d9fdaee6b57b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-18 00:36:21 +0600

    add routing for Home, LogIn, Kitchen, and Register pages

--- src/Routers/mainRoutes.jsx
+++ src/Routers/mainRoutes.jsx
@@ -1,6 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 import RootLayout from '../layouts/RootLayout';
 import Error from '../pages/Error/Error';
+import Home from '../pages/Home/Home';
+import LogIn from '../pages/LogIn/LogIn';
+import PrivateRoute from './PrivateRoute';
+import Kitchen from '../pages/Kitchen/Kitchen';
+import Register from '../pages/Register/Register';
 
 const mainRoutes =  createBrowserRouter([
     {
@@ -14,7 +19,7 @@ const mainRoutes =  createBrowserRouter([
         },
         {
           path: "login",
-          element: <Login></Login>,
+          element: <LogIn></LogIn>,
         },
         {
           path: "kitchen",
