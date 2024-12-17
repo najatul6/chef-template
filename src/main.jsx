9069dea36ec6fdb78a62023a30d20402f2f62712
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-18 00:07:30 +0600

    add HelmetProvider to main.jsx for improved document head management

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,18 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { HelmetProvider } from "react-helmet-async";
 const client = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={client}>
+    <HelmetProvider>
       <AuthProvider>
         <ToastContainer>
           <RouterProvider router={mainRoutes}></RouterProvider>
         </ToastContainer>
       </AuthProvider>
+      </HelmetProvider>
     </QueryClientProvider>
   </StrictMode>
 );
