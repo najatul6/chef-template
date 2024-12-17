c6ebaace93c9a9c18aad8c6f8efff7533a4be20c
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-18 00:26:26 +0600

    add Banner component to Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import Banner from "../components/Banner";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Banner></Banner>
+    </>
+  );
+};
 
-export default Home
+export default Home;
