b9ac2a5e060ce2468abfa06b17fc68b9d4d277d6
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-18 00:05:34 +0600

    update favicon and title in index.html; format main.jsx for consistency

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { RouterProvider } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 const client = new QueryClient();
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <QueryClientProvider client={client}>
+    <QueryClientProvider client={client}>
       <AuthProvider>
-        <ToastContainer >
-        <RouterProvider router={mainRoutes}></RouterProvider>
-      </ToastContainer>
+        <ToastContainer>
+          <RouterProvider router={mainRoutes}></RouterProvider>
+        </ToastContainer>
       </AuthProvider>
     </QueryClientProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
