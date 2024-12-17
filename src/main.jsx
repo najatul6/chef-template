2a758983551d5d66379bbfebf6a3aa5e9afcfd28
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-18 00:55:01 +0600

    fix: reorder component hierarchy in main.jsx for improved structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { HelmetProvider } from "react-helmet-async";
 import mainRoutes from "./Routers/mainRoutes";
 import AuthProvider from "./providers/AuthProvider";
+import "./index.css";
 
 const client = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={client}>
-      <HelmetProvider>
-        <AuthProvider>
-          <ToastContainer>
+    <HelmetProvider>
+      <ToastContainer>
+        <QueryClientProvider client={client}>
+          <AuthProvider>
             <RouterProvider router={mainRoutes}></RouterProvider>
-          </ToastContainer>
-        </AuthProvider>
-      </HelmetProvider>
-    </QueryClientProvider>
+          </AuthProvider>
+        </QueryClientProvider>
+      </ToastContainer>
+    </HelmetProvider>
   </StrictMode>
 );
