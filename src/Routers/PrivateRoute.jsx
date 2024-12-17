b8b4b3f3323daf0da023e49770904c306a5b11d9
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-18 00:10:42 +0600

    implement PrivateRoute component for protected routing with loading state handling

--- src/Routers/PrivateRoute.jsx
+++ src/Routers/PrivateRoute.jsx
@@ -1,8 +1,24 @@
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router";
+import { AuthContext } from "../providers/AuthProvider";
+import Loading from "../pages/Loading";
 
-const PrivateRoute = () => {
-  return (
-    <div>PrivateRoute</div>
-  )
-}
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  // console.log(location.pathname);
 
-export default PrivateRoute
+  if (loading) {
+    return <Loading></Loading>;
+  }
+
+  if (user?.email) {
+    return children;
+  }
+
+  return <Navigate state={location?.pathname} to="/login" replace></Navigate>;
+};
+
+
+
+export default PrivateRoute;
