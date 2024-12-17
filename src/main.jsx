d84fc4e10051da2b67095b13b24e6711b9af0439
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-17 23:56:14 +0600

    update styles and integrate QueryClientProvider with RouterProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider } from 'react-router-dom';
 const client = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
