96efa6ae45a1dd71bff6c8653114225ba1fd6ce9
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-18 00:03:14 +0600

    add ToastContainer to main.jsx for toast notifications

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 const client = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <QueryClientProvider client={client}>
       <AuthProvider>
+        <ToastContainer >
         <RouterProvider router={mainRoutes}></RouterProvider>
+      </ToastContainer>
       </AuthProvider>
     </QueryClientProvider>
   </StrictMode>,
